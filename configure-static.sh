f629e35a7ebe9b6b3279f71c7f04575e3d05f27f
Phil Grim <phil@insufficient-light.com>
2022-01-24 21:26:32 -0500

    Added convenience script for building.

---
+++ configure-static.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Copyright (c) 2017-2022 Thought Networks LLC
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+set -e
+set -x
+
+BASEDIR=$(pwd)
+
+if [ "x${HOST}y" = "xy" ]; then
+	export HOST="$(./depends/config.guess)"
+fi
+
+(cd depends && make)
+
+./autogen.sh
+
+export CONFIG_SITE="$BASEDIR/depends/$HOST/share/config.site"
+./configure --prefix=/ $@
