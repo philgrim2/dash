81ab9b4b498029127bfdf771b3ceaa48352d3ede
Phil Grim <phil@insufficient-light.com>
2022-02-28 11:29:17 -0500

    Renaming/rebranding work.

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -7,26 +7,26 @@ BUILDDIR=${BUILDDIR:-$TOPDIR}
 BINDIR=${BINDIR:-$BUILDDIR/src}
 MANDIR=${MANDIR:-$TOPDIR/doc/man}
 
-BITCOIND=${BITCOIND:-$BINDIR/dashd}
-BITCOINCLI=${BITCOINCLI:-$BINDIR/dash-cli}
-BITCOINTX=${BITCOINTX:-$BINDIR/dash-tx}
-BITCOINQT=${BITCOINQT:-$BINDIR/qt/dash-qt}
+THOUGHTD=${THOUGHTD:-$BINDIR/thoughtd}
+THOUGHTCLI=${THOUGHTCLI:-$BINDIR/thought-cli}
+THOUGHTTX=${THOUGHTTX:-$BINDIR/thought-tx}
+THOUGHTQT=${THOUGHTQT:-$BINDIR/qt/thought-qt}
 
-[ ! -x $BITCOIND ] && echo "$BITCOIND not found or not executable." && exit 1
+[ ! -x $THOUGHTD ] && echo "$THOUGHTD not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-BTCVER=($($BITCOINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+THTVER=($($THOUGHTCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
-# This gets autodetected fine for dashd if --version-string is not set,
-# but has different outcomes for dash-qt and dash-cli.
+# This gets autodetected fine for thoughtd if --version-string is not set,
+# but has different outcomes for thought-qt and thought-cli.
 echo "[COPYRIGHT]" > footer.h2m
-$BITCOIND --version | sed -n '1!p' >> footer.h2m
+$THOUGHTD --version | sed -n '1!p' >> footer.h2m
 
-for cmd in $BITCOIND $BITCOINCLI $BITCOINTX $BITCOINQT; do
+for cmd in $THOUGHTD $THOUGHTCLI $THOUGHTTX $THOUGHTQT; do
   cmdname="${cmd##*/}"
-  help2man -N --version-string=${BTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${BTCVER[1]}//g" ${MANDIR}/${cmdname}.1
+  help2man -N --version-string=${THTVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${THTVER[1]}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
