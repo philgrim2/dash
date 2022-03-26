fd93ca3231e5c5654205f400614bfe05c9239972
Phil Grim <phil@insufficient-light.com>
2022-03-26 13:05:05 -0400

    Replace Dash seeds with Thought ones.

--- contrib/seeds/generate-seeds.py
+++ contrib/seeds/generate-seeds.py
@@ -128,10 +128,10 @@ def main():
     g.write(' * IPv4 as well as onion addresses are wrapped inside an IPv6 address accordingly.\n')
     g.write(' */\n')
     with open(os.path.join(indir,'nodes_main.txt'), 'r', encoding="utf8") as f:
-        process_nodes(g, f, 'pnSeed6_main', 9999)
+        process_nodes(g, f, 'pnSeed6_main', 10618)
     g.write('\n')
     with open(os.path.join(indir,'nodes_test.txt'), 'r', encoding="utf8") as f:
-        process_nodes(g, f, 'pnSeed6_test', 19999)
+        process_nodes(g, f, 'pnSeed6_test', 11618)
     g.write('#endif // THOUGHT_CHAINPARAMSSEEDS_H\n')
 
 if __name__ == '__main__':
